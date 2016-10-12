01d8a2fee7a20eb2e9438be54d929bb01ce01424
Student <student@Admins-Mac-mini-24.local>
2016-10-12 11:55:17 -0600

    Incorporating OOP into exercise

--- main.js
+++ main.js
@@ -4,40 +4,59 @@ angular.module('strainKeeper', ['checklist-model'])
 function mainController() {
   var main = this;
 
+  // GOOD EFFECTS USERS CAN CHOOSE FROM ADD NEW STRAIN FORM
   main.effects = [
     'Happy',
     'Hungry',
-    'Sleepy',
+    'Energetic',
     'Relaxed',
     'Creative',
     'Euphoric'
   ]
 
+  // BAD EFFECTS TO CHOOSE FROM ADD NEW STRAIN FORM
   main.negEffects = [
-    'Dry Mouth/Eyes',
+    'Dry Mouth',
+    'Red Eyes',
     'Paranoid',
-    'Anxious'
+    'Anxious',
+    'Tired'
   ]
 
-  main.goodEffects = [];
-  main.badEffects = [];
+  // INITIALLY THERE IS NO STRAINS FOR NG-IF
+  main.isThereStrains = false;
 
+  // CONSTRUCTOR FOR A NEW STRAIN OBJECT
+  function NewStrain(name, type, rating, goodEffects, badEffects) {
+    this.name = name;
+    this.type = type;
+    this.rating = rating;
+    this.goodEffects = goodEffects;
+    this.badEffects = badEffects;
+  }
+
+  // EMPTY ARRAY WHERE THE NEW STRAINS WILL BE PUSHED
+  main.strainArray = [];
+
+  // FUNCTION CALLED WHEN SAVE BUTTON IN MODAL IS CLICKED
   main.addStrain = function() {
     $('#myModal').modal('hide');
-    $(".strainList").append("<div class='strain'><h1>" + main.strain + "</h1><h3>" + main.type + "</h3>" +
-    "<div class='eff'><h5>The Good:</h5><aside>" + main.goodEffects + "</aside><h5 class='neg'>The Bad:</h5>" +
-    "<aside>" + main.badEffects + "</aside></div>");
-    if (main.rating == 5) {
-        $('.strain:last-child').append("<img src='cannabis1.svg'><img src='cannabis1.svg'>" +
-                                      "<img src='cannabis1.svg'><img src='cannabis1.svg'><img src='cannabis1.svg'>");
-    } else if (main.rating == 4) {
-        $('.strain:last-child').append("<img src='cannabis1.svg'><img src='cannabis1.svg'><img src='cannabis1.svg'><img src='cannabis1.svg'>");
-    } else if (main.rating == 3) {
-        $('.strain:last-child').append("<img src='cannabis1.svg'><img src='cannabis1.svg'><img src='cannabis1.svg'>");
-    } else if (main.rating == 2) {
-        $('.strain:last-child').append("<img src='cannabis1.svg'><img src='cannabis1.svg'>");
-    } else {
-      $('.strain:last-child').append("<img src='cannabis1.svg'>");
-    }
-}
+
+    // INSTANTIATE A NEW STRAIN FROM CONSTRUCTOR
+    var newStrain = new NewStrain(main.strain, main.type, main.rating, main.goodEffects, main.badEffects);
+
+    // PUSH THE NEW STRAIN OBJECT ONTO THE STRAIN ARRAY
+    main.strainArray.push(newStrain);
+    console.log(main.strainArray);
+
+    // IF THE STRAIN ARRAY HAS ITEMS SET VARIABLE TO TRUE FOR NG-IF
+    if (main.strainArray.length > 0) { main.isThereStrains = true; }
+
+    // RESET ALL STRAIN VALUES
+    main.strain = '';
+    main.type = '';
+    main.rating = 0;
+    main.goodEffects = [];
+    main.badEffects = [];
+  }
 }
