30c55b17e782c602a928449c1143c9818b73470f
Student <student@Admins-Mac-mini-24.local>
2016-10-13 15:10:41 -0600

    Added local storage functionality

--- main.js
+++ main.js
@@ -23,9 +23,6 @@ function mainController() {
     'Tired'
   ]
 
-  // INITIALLY THERE IS NO STRAINS FOR NG-SHOW
-  main.isThereStrains = false;
-
   // CONSTRUCTOR FOR A NEW STRAIN OBJECT
   function NewStrain(name, type, rating, goodEffects, badEffects) {
     this.name = name;
@@ -35,11 +32,66 @@ function mainController() {
     this.badEffects = badEffects;
   }
 
-  // FILTER
+  // CREATE REFERENCE TO ARRAY IN STORAGE
+  var arrayFromStorage = JSON.parse(localStorage.getItem('strainArray'));
+  console.log(arrayFromStorage);
+
+  //IF ARRAY IN STORAGE EXIST, THEN STRAINARRAY
+  if (arrayFromStorage != null) {
+    main.strainArray = arrayFromStorage;
+    console.log("I got it from storage");
+  } else {
+    main.strainArray = [];
+    console.log("There was nothing in storage");
+  }
+
+  // INITIALLY THERE IS(true) OR IS NOT(false) STRAINS FOR NG-SHOW
+  main.isThereStrains = true;
+
+  // FILTER OBJECT
   main.strainFilter = {};
 
-  // EMPTY ARRAY WHERE THE NEW STRAINS WILL BE PUSHED
-  main.strainArray = [];
+  // ARRAY WHERE THE NEW STRAINS WILL BE KEPT
+  // main.strainArray = [
+  //   {
+  //   name: 'Blue Dream',
+  //   type: 'Hybrid',
+  //   rating: 3,
+  //   goodEffects: ['Happy', 'Creative', 'Energetic'],
+  //   badEffects: ['Red Eyes']
+  // }, {
+  //   name: 'Gorilla Glue',
+  //   type: 'Indica',
+  //   rating: 4,
+  //   goodEffects: ['Relaxed', 'Euphoric'],
+  //   badEffects: ['Tired']
+  // }, {
+  //   name: 'Jedi Kush',
+  //   type: 'Indica',
+  //   rating: 4,
+  //   goodEffects: ['Happy', 'Hungry'],
+  //   badEffects: ['Tired']
+  // }, {
+  //   name: 'Matanuska Thunder Fuck',
+  //   type: 'Sativa',
+  //   rating: 4,
+  //   goodEffects: ['Happy', 'Hungry'],
+  //   badEffects: ['Dry Mouth']
+  // }, {
+  //   name: 'Flo',
+  //   type: 'Sativa',
+  //   rating: 3,
+  //   goodEffects: ['Energetic', 'Hungry'],
+  //   badEffects: ['Red Eyes']
+  // }, {
+  //   name: 'Girl Scout Cookies',
+  //   type: 'Hybrid',
+  //   rating: 2,
+  //   goodEffects: ['Happy', 'Relaxed'],
+  //   badEffects: ['Anxious']
+  // },
+  //
+  // ];
 
   // INITIAL TYPE FILTER IS SET TO ALL
   main.typeFilter = 1;
@@ -66,7 +118,7 @@ function mainController() {
         main.typeFilter = 1;
     }
 
-    // CLICKING ON ANY FILTER BUT ALL WILL FILTER BY THAT STRAIN TYPE
+    // CLICKING ON ANY FILTER EXCEPT 'ALL' WILL FILTER BY THAT STRAIN TYPE
     if (clickedFilter != 'All'){
       main.strainFilter.type = clickedFilter;
     } else {
@@ -84,7 +136,17 @@ function mainController() {
 
     // PUSH THE NEW STRAIN OBJECT ONTO THE STRAIN ARRAY
     main.strainArray.push(newStrain);
-    localStorage.setItem('newStrain', JSON.stringify(newStrain));
+
+    // SORT ARRAY BY RATING
+    main.strainArray.sort(function(obj1, obj2){
+      return obj2.rating - obj1.rating;
+    });
+
+    // SEND ARRAY TO LOCAL STORAGE - OVERRIDES PREVIOUS ARRAY IN STORAGE WITH SAME NAME
+    localStorage.setItem('strainArray', JSON.stringify(main.strainArray));
+    // var arrayFromStorage = localStorage.getItem('strainArray');
+    // console.log('arrayFromStorage: ', JSON.parse(arrayFromStorage));
+    // console.log(main.strainArray);
 
     // IF THE STRAIN ARRAY HAS ITEMS SET VARIABLE TO TRUE FOR NG-SHOW
     if (main.strainArray.length > 0) { main.isThereStrains = true; }
