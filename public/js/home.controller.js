2b7184d83cbc817351998c4619e4d081e443c886
Sean Birdsall <sbirdsall0312@gmail.com>
2016-11-12 15:41:43 -0700

    Adding average rating logic to strain list

--- public/js/home.controller.js
+++ public/js/home.controller.js
@@ -15,6 +15,45 @@ function homeController(strainFactory) {
       home.allStrains = res.data;
       home.homeStrains = res.data;
 
+      home.filteredStrains = [];
+
+      var seen = {};
+    home.homeStrains = home.homeStrains.filter(function(entry) {
+      var previous;
+      home.allDupes = [];
+
+     // Have we seen this label before?
+     if (seen.hasOwnProperty(entry.dataName)) {
+         // Yes, grab it and add this data to it
+         previous = seen[entry.dataName];
+         previous.goodEffects.push(entry.goodEffects);
+         previous.rating.push(entry.rating);
+         home.allDupes.push(previous)
+         // Don't keep this entry, we've merged it into the previous one
+         return false;
+     }
+
+     // entry.data probably isn't an array; make it one for consistency
+     if (!Array.isArray(entry.rating)) {
+       entry.rating = [entry.rating];
+     }
+
+     // Remember that we've seen it
+     seen[entry.dataName] = entry;
+
+     // Keep this one, we'll merge any others that match into it
+     return true;
+ });
+
+      console.log(home.allDupes);
+
+      for (var i = 0; i < home.homeStrains.length; i++){
+
+      home.homeStrains[i].avgRating = home.homeStrains[i].rating.reduce(function(a, b){return a+b;}, 0) / home.homeStrains[i].rating.length;
+      console.log(home.homeStrains[i].name, home.homeStrains[i].goodEffects, home.homeStrains[i].rating, home.homeStrains[i].avgRating);
+      }
+
+      // if statement to deteremine how many strains to show on landing page depending on device width
       if (viewWidth < 675) {
 
         for (var i = 0; i < 3; i++){
